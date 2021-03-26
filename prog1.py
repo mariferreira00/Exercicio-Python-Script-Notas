528f438e321c7f719eab932f0695c118cc334c75
Maria Ferreira <81054753+mariferreira00@users.noreply.github.com>
2021-03-26 18:39:23 -0300

    Update prog1.py

--- prog1.py
+++ prog1.py
@@ -1,3 +1,4 @@
+# Pergunta ao usuario uma nota de 0  a 10 e emite uma mensagem de erro quando  é digitada uma nota inválida.
 nota = float(input("Digite uma nota entre 0 e 10: "))
 while (nota <= 0 or nota > 10):
     nota = float(input("Nota inválida. Digite uma nota entre 0 e 10: "))
